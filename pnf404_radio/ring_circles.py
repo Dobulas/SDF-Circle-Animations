d1bdb00d3f3900c91a8b3893d60d4a10efcec90b
Dobulas <148472604+Dobulas@users.noreply.github.com>
2025-08-27 20:30:51 -0500

    Fix palette shortcuts being garbage collected

--- pnf404_radio/ring_circles.py
+++ pnf404_radio/ring_circles.py
@@ -124,6 +124,7 @@ def run() -> int:
 
     executor = ThreadPoolExecutor(max_workers=1)
     pending_palette: Optional[int] = None
+    palette_shortcuts: List[QtWidgets.QShortcut] = []
 
     def _compute_images(index: int) -> List[np.ndarray]:
         """Generate RGBA sprites for the palette at ``index``."""
@@ -158,6 +159,7 @@ def run() -> int:
     for i in PALETTES:
         shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(str(i)), win)
         shortcut.activated.connect(lambda i=i: apply_palette(i))
+        palette_shortcuts.append(shortcut)
 
     ring_radius = 450
     boundary_x = width / 2 - 20
